f7e734115065a3caadb6f7bbaa0688c14d7d36bb
Petros Ring <me@petros789.com>
2018-02-20 21:44:38 -0500

    Figured out how to do voting

--- README.md
+++ README.md
@@ -6,9 +6,8 @@ Proposed by Vitalik Buterin - https://ethresear.ch/t/explanation-of-daicos/465
 This is an working version that has not been audited, use at your own risk.
 
 ### To Install
-`npm init`
-
-`npm install zeppelin-solidity`
+`npm init
+npm install zeppelin-solidity`
 
 
 ### Functionality
--- contracts/DAICO.sol
+++ contracts/DAICO.sol
@@ -26,16 +26,16 @@ contract DAICO is Ownable {
 	event ManagerAdded();
 	event ManagerRemoved();
 	event ProjectShutdownProposed();
-	event ProjectshutdownSuccess();
+	event ProjectShutdownSuccess();
 
 	/** 
 	 * DAO State Variables
 	 */
-	ERC827Token public token;
-	uint256 public tap;
-	uint256 public lastWithdrawn;
-	uint256 public quorumPercentage;
-	uint256 public proposalExpiration;
+	ERC827Token public token; 
+	uint256 public tap; // amount in wei that can be withdrawn every 7 days
+	uint256 public lastWithdrawn; // epoch date for proposal expiry
+	uint256 public quorumPercentage; // 0-100 percentage
+	uint256 public proposalExpiration; // seconds for proposal to expire
 
 	/** 
 	 * Management Variables
@@ -47,6 +47,13 @@ contract DAICO is Ownable {
 	 * Proposal Variables
 	 */
 	Proposals[] public proposals;
+	mapping(uint256 => mapping (address => bool)) public responses;
+
+	/** 
+	 * Token Holder Variables
+	 */
+	uint256 minimumTokens; // Mininum token amount to vote for an address
+	mapping(address => uint256) holderStartDate; // Date token holder registered
 
 	/** 
 	 * Contract System State Variables
@@ -61,6 +68,12 @@ contract DAICO is Ownable {
 		address addr;
 		string name;
 		string title;
+		bool active;
+	}
+
+	struct Proposal {
+		address creator;
+
 	}
 
 	/** 
@@ -72,28 +85,30 @@ contract DAICO is Ownable {
 	}
 
 	modifier onlyTokenHolder {
-		require(token.balanceOf(msg.sender) > 0);
+		require(token.balanceOf(msg.sender) > minimumTokens);
 		_;
 	}
 
 	/** 
 	 * Constructor function
 	 */
-	function DAICO() {
+	function DAICO(uint256 _mininumTokens) {
 		// Set DAICO State to Setup Mode
 		daicoState = FundraisingState.OrginizationSetup;
 		// Set up first manager as contract creator
 		Management firstManager = Management(msg.sender, '', '');
 		managers.add(firstManager);
 		managerLookup[msg.sender] = true;
+		// Set Mininum token amount
+		minimumTokens = _mininumTokens;
 	}
 
 	/**
 	 * @dev Fundraising Endpoint (Default Payable) to accept funds for ICO
 	 */
-	function () payable {
+	function () public payable {
 		// Check that DAICO is in fundraising state
-		require(msg.value > 0 && daicoState == FundraisingState.FundraisingStart)
+		require(msg.value > 0 && daicoState == FundraisingState.FundraisingStart);
 		// TODO: Perform Token Sale Actions
 
 		// Send Event to Frontend
@@ -106,31 +121,44 @@ contract DAICO is Ownable {
 	 * @param name Name of the new manager
 	 * @param title Title/Position of the new manager
 	 */
-	function addManager(address addr, string name, string title) onlyManagement {}
+	function addManager(address addr, string name, string title) public onlyManagement {}
 
 	/**
 	 * @dev Allows current managers to vote to remove a new manager, requires 2/3 consensus
 	 * @param addr Address of the manager
 	 */
-	function removeManager(address addr) onlyManagement {}
+	function removeManager(address addr) public onlyManagement {}
 
 	/** 
 	 * @dev Allows managers to propose a fund request from the investors
 	 * @param 
 	 */
-	function createProposal() onlyManagement {}
+	function createProposal() public onlyManagement {}
 
 	/**
 	 * @dev Allows token holders to vote on proposal requests
 	 * @param 
-	 * 
-	 * 
 	 */
-	function proposalVote(uint propsalId, bool action, string comments) onlyTokenHolder {}
+	function proposalVote(uint propsalId, bool action, string comments) public onlyTokenHolder {}
 
-	function proposeNewQuorumPercentage() {}
+	/**
+	 * @dev  
+	 */
+	function proposeProjectShutdown() public onlyTokenHolder {}
 
-	function proposeExpirationPeriod() {}
+	/**
+	 * @dev Register token holding address to be able to vote
+	 */
+	function registerAddress() public onlyTokenHolder {
+		holderStartDate[msg.sender] = block.timestamp;
+	}
 
-	function proposeProjectShutdown() {}
+	/**
+	 * @dev Address must have register date before _earlistDate
+	 * @param addr Address of token holder
+	 * @param earliestDate the earliest date they may have been registered
+	 */
+	function beforeDate(address addr, uint256 earliestDate) internal {
+		require(holderStartDate[addr] < earliestDate);
+	}
 }
